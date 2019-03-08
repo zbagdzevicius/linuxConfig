e9009bb3b6a98790c0064c553a581ff87b08db3f
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-08 15:16:32 +0200

    A

---
+++ extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension christian-kohler.path-intellisense
+code --install-extension eg2.tslint
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension k--kato.intellij-idea-keybindings
+code --install-extension Mikael.Angular-BeastCode
+code --install-extension ms-python.python
+code --install-extension rbbit.typescript-hero
