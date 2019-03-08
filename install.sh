fc2679409382d5c05c9f7d88806694d805a5c5f4
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-08 15:11:18 +0200

    A

---
+++ install.sh
@@ -0,0 +1,19 @@
+sudo apt update -y 
+sudo apt upgrade -y
+sudo apt install git -y
+git config --global user.name "zbagdzevicius"
+git config --global user.email "zbagdzevicius@gmail.com"
+git config --global credentials.helper cache 999999999
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo add-apt-repository universe -y
+sudo apt install gnome-tweak-tool -y
+sudo add-apt-repository ppa:linrunner/tlp -y
+sudo apt-get update -y
+sudo apt-get install tlp tlp-rdw -y
+sudo tlp start -y
+sudo apt install python3-pip -y
+sudo apt install postgresql postgresql-contrib -y
+sudo apt install pgadmin3 -y
+sudo snap install postman
+sudo snap install filezilla -y
